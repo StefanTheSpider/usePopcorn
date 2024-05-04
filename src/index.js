2bdb9f45635ebe294165eb7a047eac3033541b14
StefanTheSpider <stefantuaev@gmail.com>
2024-05-04 15:01:33 +0200

    added props to make this component reuseble

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './starrating';
+
+function Test() {
+    const [movieRating, setMovieRating] = useState(0);
+    return (
+        <div>
+            <StarRating maxRating={5} onSetRating={setMovieRating} />
+            <p>has {movieRating} stars</p>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+        <StarRating
+            maxRating={10}
+            className={'className'}
+            messages={['bad', 'ok', 'good', 'super', 'exelent']}
+            defaultRating={0}
+        />
+
+        <Test />
+    </React.StrictMode>
 );
