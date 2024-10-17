45ea9cc1f86dda7005214bd75065e87bc6b233e3
StefanTheSpider <stefantuaevgmail.com>
2024-10-17 16:54:29 +0200

    search input improved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import StarRating from './starrating.js';
 
 const average = (arr) =>
@@ -312,6 +312,25 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+
+    useEffect(
+        function () {
+            function callback(e) {
+                if (document.activeElement === inputEl.current) return;
+
+                if (e.code === 'Enter') {
+                    inputEl.current.focus();
+                    setQuery('');
+                }
+            }
+
+            document.addEventListener('keydown', callback);
+            return () => document.addEventListener('keydown', callback);
+        },
+        [setQuery]
+    );
+
     return (
         <input
             className="search"
@@ -319,6 +338,7 @@ function Search({ query, setQuery }) {
             placeholder="Search movies..."
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
         />
     );
 }
